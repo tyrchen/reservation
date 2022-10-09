7a7f4593dc9b90b767606e726636a1574eacb6d2
Tyr Chen <tyr.chen@gmail.com>
2022-10-09 07:22:38 -0700

    update comment

--- migrations/20221008222343_init.up.sql
+++ migrations/20221008222343_init.up.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA rsvp;
--- AWS RDS support this: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts.General.Extensions.Trusted
+-- AWS RDS support this: https://docs.aws.amazon.com/AmazonRDS/latest/PostgreSQLReleaseNotes/postgresql-extensions.html
 CREATE EXTENSION btree_gist;
 
 -- TODO: consider to create a role for the application
